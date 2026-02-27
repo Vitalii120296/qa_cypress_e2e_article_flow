d9085580373a2b33168e2e25202872b889356b13
Vitalii <v.hulaievych@gmail.com>
2026-02-27 06:38:17 +0000

    solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,39 @@
+/// <reference types='cypress' />
+
+const { generateUser } = require('../support/e2e');
+
 describe('', () => {
   before(() => {
-
+    cy.visit('/');
   });
 
-  it('', () => {
+  it('create the article', () => {
+    const { email, username, password } = generateUser();
 
+    cy.login(email, username, password);
+
+    const article = {
+      title: `title${Date.now()}`,
+      description: `Description${Date.now()}`,
+      body: `Any text ${Date.now()}`
+    };
+
+    cy.createArticle(article.title, article.description, article.body);
   });
 
-  it('', () => {
+  it.skip('delete the article', function () {
+    const { email, username, password } = generateUser();
+    cy.login(email, username, password);
 
+    const article = {
+      title: `title${Date.now()}`,
+      description: `Description${Date.now()}`,
+      body: `Any text ${Date.now()}`
+    };
+    cy.createArticle(article.title, article.description, article.body).then(
+      (slug) => {
+        cy.deleteArticle(slug);
+      }
+    );
   });
 });
